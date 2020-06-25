318b88dbc849827145ba18ed72d876fbe461f845
Cardoso <lsc.cardosoo@gmail.com>
2020-06-24 23:34:11 -0300

    Codeclimate 1 para o subcomponente Welcome

--- src/pages/Login/Welcome/Welcome.js
+++ src/pages/Login/Welcome/Welcome.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Welcome.css';
+import woman from './mulher.svg'
+import man from './pessoa.svg';
 
 const Welcome = () => (
   <div className="welcome-container">
     <h1>Welcome</h1>
     <div>
-      <img className="personal-icon" src={require('./pessoa.svg')} alt="icone de um homem"/>
-      <img className="personal-icon" src={require('./mulher.svg')} alt="icone de uma mulher"/>
+      <img className="personal-icon" src={woman} alt="icone de um homem" />
+      <img className="personal-icon" src={man} alt="icone de uma mulher" />
     </div>
   </div>
 );
