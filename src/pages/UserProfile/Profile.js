91566380149a96afa01967e6ab22036395b08aa8
Cardoso <lsc.cardosoo@gmail.com>
2020-06-25 11:52:41 -0300

    Codeclimate 1 para tags no arquivo Profile.js

--- src/pages/UserProfile/Profile.js
+++ src/pages/UserProfile/Profile.js
@@ -5,23 +5,23 @@ const Profile = () => {
   const [email, setEmail] = useState('');
 
   useEffect(() => {
-   setEmail(JSON.parse(localStorage.getItem('user')).email)
-  },[]);
+    setEmail(JSON.parse(localStorage.getItem('user')).email);
+  }, []);
 
   return (
     <div>
       <span data-testid="profile-email">{email}</span>
       <Link to="/receitas-feitas">
-        <button data-testid="profile-done-btn">
-Receitas Feitas</button>
+        <button data-testid="profile-done-btn">Receitas Feitas</button>
       </Link>
       <Link to="/receitas-favoritas">
         <button data-testid="profile-favorite-btn">Receitas Favoritas</button>
       </Link>
       <Link to="/">
         <button
-          onClick={() => localStorage.clear()}data-testid="profile-logout-btn">
-            Sair
+          onClick={() => localStorage.clear()} data-testid="profile-logout-btn"
+        >
+          Sair
         </button>
       </Link>
     </div>
