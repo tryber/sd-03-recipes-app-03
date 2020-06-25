e0a5214c223afb1c048c54f30e3415257e5618d9
Cardoso <lsc.cardosoo@gmail.com>
2020-06-25 11:47:47 -0300

    Incluir email na tela de Perfil e tres botoes que redirecionam para as rotas de inicio, receitas favoridadas e receitas feitas

---
+++ src/pages/UserProfile/Profile.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const Profile = () => {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+   setEmail(JSON.parse(localStorage.getItem('user')).email)
+  },[]);
+
+  return (
+    <div>
+      <span data-testid="profile-email">{email}</span>
+      <Link to="/receitas-feitas">
+        <button data-testid="profile-done-btn">
+Receitas Feitas</button>
+      </Link>
+      <Link to="/receitas-favoritas">
+        <button data-testid="profile-favorite-btn">Receitas Favoritas</button>
+      </Link>
+      <Link to="/">
+        <button
+          onClick={() => localStorage.clear()}data-testid="profile-logout-btn">
+            Sair
+        </button>
+      </Link>
+    </div>
+  );
+};
+
+export default Profile;
