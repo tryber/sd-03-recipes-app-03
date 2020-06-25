34722387c61306b53d3c1ea881c25fb0246fb113
Cardoso <lsc.cardosoo@gmail.com>
2020-06-25 16:56:22 -0300

    Codeclimate 1: Arquivo de requisicao

---
+++ src/services/theCockTailAPI.js
@@ -0,0 +1,55 @@
+const url12Drinks = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+export const fetchDrinks = (quantity) => (
+  fetch(`${url12Drinks}${quantity}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
+
+const urlCategoriesDrinks = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+export const fetchCategoriesDrinks = () => (
+  fetch(urlCategoriesDrinks)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
+
+const urlCategoryDrinksButton = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=';
+export const fetchDrinkByCategory = (category) => (
+  fetch(`${urlCategoryDrinksButton}${category}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
+
+const urlDrinkById = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+export const fetchDrinkById = (id) => (
+  fetch(`${urlDrinkById}${id}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
+
+const urlRandomDrink = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+export const fetchRandomDrink = () => (
+  fetch(urlRandomDrink)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
+
+const urlListDrinksIngredients = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+export const fetchDrinksByCategory = () => (
+  fetch(urlListDrinksIngredients)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
+
+const urlFindDrinkByIngredients = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
+export const fetchMealsByCategory = (ingredient) => (
+  fetch(`${urlFindDrinkByIngredients}${ingredient}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
+
+const urlFindDrinksByName = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+export const fetchMealsByName = (name) => (
+  fetch(`${urlFindDrinksByName}${name}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
--- src/services/theMealAPI.js
+++ src/services/theMealAPI.js
@@ -1,76 +1,76 @@
-const url_12meals = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
-export const fetchMeals = (quantity) => {
-  return fetch(`${url_12meals}${quantity}`)
+const url12Meals = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+export const fetchMeals = (quantity) => (
+  fetch(`${url12Meals}${quantity}`)
     .then((response) => response.json())
-    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-};
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlCategoriesMeals = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
-export const fetchCategoriesMeals = () => {
-  return fetch(urlCategoriesMeals)
+export const fetchCategoriesMeals = () => (
+  fetch(urlCategoriesMeals)
   .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlCategoryMealsButton = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=';
-export const fetchMealsByCategory = (category) => {
-  return fetch(`${urlCategoryMealsButton}${category}`)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchMealsByCategory = (category) => (
+  fetch(`${urlCategoryMealsButton}${category}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlMealById = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=';
-export const fetchMealById = (id) => {
-  return fetch(`${urlMealById}${id}`)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchMealById = (id) => (
+  fetch(`${urlMealById}${id}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlRandomMeal = 'https://www.themealdb.com/api/json/v1/1/random.php';
-export const fetchRandomMeal = () => {
-  return fetch(urlRandomMeal)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchRandomMeal = () => (
+  fetch(urlRandomMeal)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlAreasList = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
-export const fetchAreasList = () => {
-  return fetch(urlAreasList)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchAreasList = () => (
+  fetch(urlAreasList)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlFilterByArea = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=';
-export const fetchMealsByCategory = (country) => {
-  return fetch(`${urlFilterByArea}${country}`)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchMealsByCategory = (country) => (
+  fetch(`${urlFilterByArea}${country}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlListMealsIngredients = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
-export const fetchMealsByCategory = () => {
-  return fetch(urlListMealsIngredients)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchMealsByCategory = () => (
+  fetch(urlListMealsIngredients)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlFindMealsByIngredients = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
-export const fetchMealsByCategory = (ingredient) => {
-  return fetch(`${urlFindMealsByIngredients}${ingredient}`)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchMealsByCategory = (ingredient) => (
+  fetch(`${urlFindMealsByIngredients}${ingredient}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlFindMealsByName = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
-export const fetchMealsByName = (name) => {
-  return fetch(`${urlFindMealsByName}${name}`)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchMealsByName = (name) => (
+  fetch(`${urlFindMealsByName}${name}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
 
 const urlFindByFirstLetter = 'https://www.themealdb.com/api/json/v1/1/search.php?f=';
-export const fetchMealsByFirstLetter = (letter) => {
-  return fetch(`${urlFindByFirstLetter}${letter}`)
-  .then((response) => response.json())
-  .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json));
-}
+export const fetchMealsByFirstLetter = (letter) => (
+  fetch(`${urlFindByFirstLetter}${letter}`)
+    .then((response) => response.json())
+    .then((json) => Promise.ok ? Promise.resolve(json) : Promise.reject(json))
+);
