4f996371d8bffa2c0f6b907db106299b637bed99
Cardoso <lsc.cardosoo@gmail.com>
2020-06-26 13:14:37 -0300

    Refatorar logica para switch case e callbacks para reduçao de complexidade

--- src/components/Categories/ListCategories.js
+++ src/components/Categories/ListCategories.js
@@ -4,34 +4,56 @@ import FoodContext from '../../pages/FoodMainPage/Context/FoodContext';
 import { fetchDrinkByCategoryButton } from '../../services/theCockTailAPI';
 import { fetchCategoryMealsButton } from '../../services/theMealAPI';
 
+const handleCategories = (category, fetchCallBack, setCallBack) => {
+  fetchCallBack(category)
+    .then((err) => err, (response) => setCallBack(Object.values(response)[0]));
+};
+
 const ListCategories = ({ strCategories, type }) => {
   const { setMealsData, setDrinksData, dataBase: { drinks, meals } } = useContext(FoodContext);
   const [allCategories, setAllCategories] = useState('');
   const handleAllCategories = (callback, data, category) => {
     if (allCategories.category === category || category === 'All') {
-      setAllCategories('');
       callback(data);
       return true;
     }
     return false;
   };
 
-  const handleCategories = (category, fetchCallBack, setCallBack) => {
-    setAllCategories({ category });
-    fetchCallBack(category)
-      .then((err) => err, (response) => setCallBack(Object.values(response)[0]));
-  };
+  // const filterByCategory = (category, categoryType) => {
+  //   if (categoryType === 'drink') {
+  //     if (handleAllCategories(setDrinksData, drinks, category)) {
+  //       return setAllCategories('');
+  //     };
+  //     handleCategories(category, fetchDrinkByCategoryButton, setDrinksData);
+  //     setAllCategories({ category });
+  //   } else {
+  //     if (handleAllCategories(setMealsData, meals, category)) {
+  //       return setAllCategories('');
+  //     };
+  //     handleCategories(category, fetchCategoryMealsButton, setMealsData);
+  //     setAllCategories({ category });
+  //   }
+  //   return null;
+  // };
 
   const filterByCategory = (category, categoryType) => {
-    if (categoryType === 'drink') {
-      if (handleAllCategories(setDrinksData, drinks, category)) return null;
-      handleCategories(category, fetchDrinkByCategoryButton, setDrinksData);
-    } else {
-      if (handleAllCategories(setMealsData, meals, category)) return null;
-      handleCategories(category, fetchCategoryMealsButton, setMealsData);
+    switch(categoryType) {
+      case 'drink':
+        if (handleAllCategories(setDrinksData, drinks, category)) {
+          return setAllCategories('');
+        } else {
+          handleCategories(category, fetchDrinkByCategoryButton, setDrinksData);
+          setAllCategories({ category });
+        }
+      case 'meal':
+        if (handleAllCategories(setMealsData, meals, category)) {
+          return setAllCategories('');
+        };
+        handleCategories(category, fetchCategoryMealsButton, setMealsData);
+        setAllCategories({ category });
+      };
     }
-    return null;
-  };
   return (
     <div>
       {strCategories.map(({ strCategory }, index) => (
