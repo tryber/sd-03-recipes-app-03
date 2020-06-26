f85d1a50be001086feaeca7fc97b49e79b4f8ac4
Jafet Henrique <jafetguerra6@gmail.com>
2020-06-26 16:55:59 -0300

    cc

--- src/pages/DetailsPage/RecipeButtonControl.js
+++ src/pages/DetailsPage/RecipeButtonControl.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const RecipeButtonControl = (props) => {
@@ -26,3 +27,17 @@ const RecipeButtonControl = (props) => {
 };
 
 export default RecipeButtonControl;
+
+RecipeButtonControl.propTypes = {
+  type: PropTypes.string,
+  id: PropTypes.number,
+  setInProgress: PropTypes.func,
+  inProgress: PropTypes.bool,
+}
+
+RecipeButtonControl.defaultProps = {
+  type: '',
+  id: undefined,
+  setInProgress: () => {},
+  inProgress: false,
+}
