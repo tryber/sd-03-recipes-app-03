35fa34a7ce22cab92d91ad04184d2c3ec34b0635
Luma Arruda <luma.oarruda@gmail.com>
2020-06-29 22:05:45 -0300

    code climate complexity changes

--- src/components/HeaderSearchBar/searchButtonFunction.js
+++ src/components/HeaderSearchBar/searchButtonFunction.js
@@ -13,6 +13,21 @@ const pushRoute = (history, type, data) => {
   if (type === 'cocktail' && data.drinks.length === 1) {
     history.push(`/bebidas/${data.drinks[0].idDrink}`);
   }
+};
+
+const performSearch = async (
+  history,
+  type,
+  setIsFetching,
+  setData,
+  searchParam,
+  searchName,
+) => {
+  const data = await search[searchParam](searchName, type);
+  setData(data.meals || data.drinks);
+  setIsFetching(false);
+  if (data.length === 0) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
+  pushRoute(history, type, data);
 }
 
 const searchButton = async (
@@ -26,11 +41,14 @@ const searchButton = async (
   if (searchParam === 'firstLetter' && searchName.length !== 1) {
     alert('Sua busca deve conter somente 1 (um) caracter');
   } else {
-    const data = await search[searchParam](searchName, type);
-    setData(data.meals || data.drinks);
-    setIsFetching(false);
-    if (data.length === 0) alert('Sinto muito, não encontramos nenhuma receita para esses filtros.');
-    pushRoute(history, type, data);
+    performSearch(
+      history,
+      type,
+      setIsFetching,
+      setData,
+      searchParam,
+      searchName,
+    )
   }
 };
 
