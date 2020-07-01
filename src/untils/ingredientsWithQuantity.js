874a332e4e1f139d08cda4b011d921bbbfc40dfb
Jafet Henrique <jafetguerra6@gmail.com>
2020-07-01 18:43:00 -0300

    cc

--- src/untils/ingredientsWithQuantity.js
+++ src/untils/ingredientsWithQuantity.js
@@ -1,11 +1,14 @@
 
-const ingredientsWithQuantity = (namesArray, quantityArray) =>
-  namesArray.reduce((acc, currentElement, index) => {
+const ingredientsWithQuantity = (namesArray, arrayOfQuantity) => {
+  const quantityArray = [...arrayOfQuantity]
+  return namesArray.reduce((acc, currentElement, index) => {
     if (currentElement !== '' && currentElement !== null) {
       quantityArray[index] = quantityArray[index] || 'A seu gosto';
       acc.push([currentElement, quantityArray[index]]);
     }
     return acc;
   }, []);
+}
+
 
 export default ingredientsWithQuantity;
