d396b424ddb9547799d93270e0107737306f193a
Cardoso <lsc.cardosoo@gmail.com>
2020-07-08 20:53:37 -0300

    cc

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './Loading.css';
+import brigadeiro from './icons/brigadeiro.png';
+import lasanha from './icons/lasanha.png';
+import vinho from './icons/vinho.png';
+import cereja from './icons/cereja.png';
+import sushi from './icons/sushi.png';
 
 const Loading = () => {
   const [icon, setIcon] = useState('brigadeiro');
 
   useEffect(() => {
-    const icons = ['brigadeiro', 'lasanha', 'cereja', 'sushi', 'vinho'];
+    const icons = [brigadeiro, lasanha, cereja, sushi, vinho];
     const interval = setInterval(() => {
-      setIcon(icons[parseInt(Math.random() * (5))]);
+      setIcon(icons[parseInt(Math.random() * (5), 10)]);
     }, 100);
     return () => clearInterval(interval);
   }, []);
   return (
     <div className="spinner">
-      <img className="loading" src={require(`./icons/${icon}.png`)} alt="icone de uma comida" />
+      <img className="loading" src={icon} alt="icone de uma comida" />
     </div>
   );
 };
