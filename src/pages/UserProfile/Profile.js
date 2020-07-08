462c1f9ff270344a835b9d7525f7003e39b98cf9
Luma Arruda <luma.oarruda@gmail.com>
2020-07-08 17:48:34 -0300

    estilizaçoes

--- src/pages/UserProfile/Profile.js
+++ src/pages/UserProfile/Profile.js
@@ -23,27 +23,29 @@ const Profile = () => {
   return (
     <div className="profile b-shadow">
       <Header title="Perfil" searchIcon={false} />
-      <label htmlFor="email">E-mail</label>
-      <span data-testid="profile-email">{email}</span>
-      <button
-        className="profile-btn"
-        onClick={() => setRote('/receitas-feitas')} data-testid="profile-done-btn"
-      >
-        Receitas Feitas
-      </button>
-      <button
-        className="profile-btn"
-        onClick={() => setRote('/receitas-favoritas')}
-        data-testid="profile-favorite-btn"
-      >
-        Receitas Favoritas
-      </button>
-      <button
-        className="profile-btn"
-        onClick={() => handleExit()} data-testid="profile-logout-btn"
-      >
-        Sair
-      </button>
+      <div className="align-container">
+        <label htmlFor="email">E-mail</label>
+        <span data-testid="profile-email">{email}</span>
+        <button
+          className="profile-btn"
+          onClick={() => setRote('/receitas-feitas')} data-testid="profile-done-btn"
+        >
+          Receitas Feitas
+        </button>
+        <button
+          className="profile-btn"
+          onClick={() => setRote('/receitas-favoritas')}
+          data-testid="profile-favorite-btn"
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          className="profile-btn"
+          onClick={() => handleExit()} data-testid="profile-logout-btn"
+        >
+          Sair
+        </button>
+      </div>
       <Footer />
     </div>
   );
