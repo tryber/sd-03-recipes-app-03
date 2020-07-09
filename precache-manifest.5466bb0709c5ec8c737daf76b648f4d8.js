af67faed7ac1c230735de85a584b630e6e0ea388
Cardoso <lsc.cardosoo@gmail.com>
2020-07-08 23:21:46 -0300

    Updates

---
+++ precache-manifest.5466bb0709c5ec8c737daf76b648f4d8.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568da52772fc4ad4985a54f5344cc2f6",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/index.html"
+  },
+  {
+    "revision": "7ef7cf6c6a9fd5988320",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/css/main.b017d5e9.chunk.css"
+  },
+  {
+    "revision": "98f22b75c865c8c9ae90",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/js/2.7845199a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/js/2.7845199a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef7cf6c6a9fd5988320",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/js/main.f86c48f3.chunk.js"
+  },
+  {
+    "revision": "fd84aac1322e4f16475c",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/js/runtime-main.46f51749.js"
+  },
+  {
+    "revision": "3c35e11f11ac4527aa692314ee71094f",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/beber.3c35e11f.svg"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "b9ef5071bb605cc7440d599ed599f969",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/chef.b9ef5071.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "8bab732c217955f87f5a9ae437ce9469",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/garfo.8bab732c.svg"
+  },
+  {
+    "revision": "8582886ca612029ccd7bf3cf071c9559",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/icones.8582886c.png"
+  },
+  {
+    "revision": "bd9121aaac3294795f1cc5daf0aa5b0f",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/loginbackground.bd9121aa.gif"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "b9ef5071bb605cc7440d599ed599f969",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/profileIcon.b9ef5071.svg"
+  },
+  {
+    "revision": "a852f82b4de845ab6d4963f16ac0a443",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/searchIcon.a852f82b.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/sd-0x-recipes-app-N.github.io/sd-03-recipes-app-03/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
